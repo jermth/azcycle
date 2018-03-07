63f50d882deafe75099fd21889ea63fbb3fd1982
Jer-Ming Chia <jer-ming.chia@cyclecomputing.com>
2018-03-07 09:18:41 -0800

    Updating the install script.

--- cyclecloud_install.py
+++ cyclecloud_install.py
@@ -2,23 +2,127 @@
 # Prepare an Azure provider account for CycleCloud usage.
 import argparse
 import tarfile
+import json
+import re
 from random import SystemRandom
-from string import ascii_letters, digits
+from string import ascii_letters, ascii_lowercase, digits
 from subprocess import call
 from os import path, makedirs, chdir, fdopen, remove
+from urllib2 import urlopen, Request
 from urllib import urlretrieve
-from shutil import rmtree, copyfile, move
+from shutil import rmtree, copy2, move
 from tempfile import mkstemp, mkdtemp
 
-print "Creating temp directory " + tmpdir + " for installing CycleCloud"
+
 tmpdir = mkdtemp()
+print "Creating temp directory " + tmpdir + " for installing CycleCloud"
 cycle_root = "/opt/cycle_server"
 
-def cleanUp():
+def clean_up():
     rmtree(tmpdir)    
 
 
-def startCC():
+def setup_azure_account(tenant_id, application_id, application_secret, cycle_portal_account, cycle_portal_pw, cyclecloud_admin_pw):
+    metadata_url = "http://169.254.169.254/metadata/instance?api-version=2017-08-01"
+    metadata_req = Request(metadata_url, headers={"Metadata" : True})
+    metadata_response = urlopen(metadata_req)
+    vm_metadata = json.load(metadata_response)
+
+    subscription_id = vm_metadata["compute"]["subscriptionId"]
+    location = vm_metadata["compute"]["location"]
+    resource_group = vm_metadata["compute"]["resourceGroupName"]
+
+    random_suffix = ''.join(SystemRandom().choice(ascii_lowercase + digits) for _ in range(8))
+
+    non_alphanu = re.compile('[\W_]+')
+    storage_account_name = non_alphanu.sub('',resource_group).lower()  + random_suffix 
+
+    azure_data = {
+        "Name": "azure",
+        "DefaultAccount": True,
+        "Provider": "azure",
+        "AzureRMSubscriptionId": subscription_id,
+        "AzureRMTenantId": tenant_id,
+        "AzureRMApplicationId": application_id,
+        "AzureRMApplicationSecret": application_secret,
+        "ProviderId": subscription_id,
+        "AzureDefaultRegion": location,
+        "AzureResourceGroup": resource_group,
+        "RMStorageAccount": storage_account_name,
+        "RMStorageContainer": "cyclecloud"
+    }
+
+    app_setting_data = {
+        "AdType": "Application.Setting",
+        "Name": "cycleserver.sendAnonymizedData",
+        "Value": True
+    }
+    app_setting_installation = {
+        "AdType": "Application.Setting",
+        "Name": "cycleserver.installation.complete",
+        "Value": True
+    }
+    authenticated_user = {
+        "AdType": "AuthenticatedUser",
+        "Name": "admin",
+        "RawPassword": cyclecloud_admin_pw,
+        "Superuser": True
+    }
+    site_name = {
+        "AdType": "Application.Setting",
+        "Name": "site_name",
+        "Value": resource_group,
+        "Category": "Support" 
+    }
+    portal_account = {
+        "AdType": "Application.Setting",
+        "Description": "The account login for this installation",
+        "Value": cycle_portal_account,
+        "Name": "support.account.login"
+    }
+    portal_login = {
+        "AdType": "Application.Setting",
+        "Description": "The account login for this installation",
+        "Value": cycle_portal_pw,
+        "Name": "support.account.password"
+    }
+
+    account_data = [
+        app_setting_data,
+        app_setting_installation,
+        authenticated_user,        
+        site_name,
+        portal_account,
+        portal_login 
+    ]
+
+    account_data_file = tmpdir + "/account_data.json"
+    azure_data_file = tmpdir + "/azure_data.json"
+
+    with open(account_data_file, 'w') as fp:
+        json.dump(account_data, fp)
+
+    with open(azure_data_file, 'w') as fp:
+        json.dump(azure_data, fp)
+
+    copy2(account_data_file, cycle_root + "/config/data/")
+
+    call(["/usr/local/bin/cyclecloud", "initialize", "--batch", "--url=https://localhost", "--verify-ssl=false", "--username=admin", "--password=" + "'" + cyclecloud_admin_pw + "'" ])    
+
+    homedir = path.expanduser("~")
+    cycle_config = homedir + "/.cycle/config.ini"
+    with open(cycle_config, "a") as config_file:
+        config_file.write("\n")
+        config_file.write("[pogo azure-storage]")
+        config_file.write("type = az")
+        config_file.write("subscription_id = " + subscription_id)
+        config_file.write("tenant_id = " + tenant_id)
+        config_file.write("application_id = " + application_id)
+        config_file.write("application_secret = " + application_secret)
+        config_file.write("matches = az://"+ storage_account_name + "/cyclecloud") 
+
+
+def start_cc():
     cs_cmd = cycle_root + "/cycle_server"
     call([cs_cmd, "start"])
     call([cs_cmd, "await_startup"])
@@ -30,7 +134,7 @@ def _sslCert(randomPW):
     call(["chmod", "600", cycle_root+"/.keystore" ])
 
 
-def modifyCSConfig():
+def modify_cs_config():
     # modify the CS config files
     cs_config_file = cycle_root + "/config/cycle_server.properties"
 
@@ -38,62 +142,66 @@ def modifyCSConfig():
     # generate a self-signed cert
     _sslCert(randomPW)
 
-    fh, abs_path = mkstemp()
+    fh, tmp_cs_config_file = mkstemp()
     with fdopen(fh,'w') as new_config:
         with open(cs_config_file) as cs_config:
             for line in cs_config:
-                if 'webServerMaxHeapSize' in line:
+                if 'webServerMaxHeapSize=' in line:
                     new_config.write('webServerMaxHeapSize=4096M')
-                elif 'webServerPort' in line:
+                elif 'webServerPort=' in line:
                     new_config.write('webServerPort=80')
-                elif 'webServerSslPort' in line:
+                elif 'webServerSslPort=' in line:
                     new_config.write('webServerSslPort=443')
-                elif 'webServerEnableHttps' in line:
+                elif 'webServerEnableHttps=' in line:
                     new_config.write('webServerEnableHttps=true')
-                elif 'webServerKeystorePass' in line:
+                elif 'webServerKeystorePass=' in line:
                     new_config.write('webServerKeystorePass=' + randomPW)
                 else:
                     new_config.write(line)
 
-    remove(cs_config)
-    move(new_config, cs_config)
+    remove(cs_config_file)
+    move(tmp_cs_config_file, cs_config_file)
+
+    #Ensure that the files are created by the cycleserver service user
+    call(["chown", "-R", "cycle_server.", cycle_root])
 
 
-def generateSSHkey():
+def generate_ssh_key():
     # create an SSH private key for VM access
     homedir = path.expanduser("~")
     sshdir = homedir + "/.ssh"
     if not path.isdir(sshdir):
-        makedirs(sshdir, mode="0700") 
+        makedirs(sshdir, mode=700) 
     
     sshkeyfile = sshdir + "/cyclecloud.pem"
     if not path.isfile(sshkeyfile):
-        call(["ssh-keygen", "-f", sshkeyfile, "-t", "rsa", "-b", "2048" "-P", '""'])
+        call(["ssh-keygen", "-f", sshkeyfile, "-t", "rsa", "-b", "2048","-P", ''])
 
     # make the cyclecloud.pem available to the cycle_server process
     cs_sshdir = cycle_root + "/.ssh"
     cs_sshkeyfile = cs_sshdir + "/cyclecloud.pem"
 
     if not path.isdir(cs_sshdir):
-        makedirs(cs_sshdir, mode="0700")
+        makedirs(cs_sshdir, mode=700)
     
     if not path.isdir(cs_sshkeyfile):
-        copyfile(sshkeyfile, cs_sshkeyfile)
+        copy2(sshkeyfile, cs_sshkeyfile)
         call(["chown", "-R", "cycle_server.", cs_sshdir])
 
 
-def ccLicense():
+def cc_license(license_url):
     # get a license
     license_file = cycle_root + '/license.dat'
-    urlretrieve(argparse.licenseURL, license_file)
+    print "Fetching temporary license from " + license_url
+    urlretrieve(license_url, license_file)
     call(["chown", "cycle_server.", license_file])
 
 
-def downloadAndInstallCC(args):    
+def download_install_cc(download_url):    
     chdir(tmpdir)
-    cc_url = args.downloadURL + "/cycle_server-all-linux64.tar.gz"
-    cli_url = args.downloadURL + "/cyclecloud-cli.linux64.tar.gz"
-    pogo_url = args.downloadURL + "/pogo-cli.linux64.tar.gz"
+    cc_url = download_url + "/cycle_server-all-linux64.tar.gz"
+    cli_url = download_url + "/cyclecloud-cli.linux64.tar.gz"
+    pogo_url = download_url + "/pogo-cli.linux64.tar.gz"
 
     print "Downloading CycleCloud from " + cc_url
     urlretrieve (cc_url, "cycle_server-all-linux64.tar.gz")
@@ -106,19 +214,22 @@ def downloadAndInstallCC(args):
     cc_tar = tarfile.open("cycle_server-all-linux64.tar.gz", "r:gz")
     cc_tar.extractall(path=tmpdir)
     cc_tar.close()
+
     
     cli_tar = tarfile.open("cyclecloud-cli.linux64.tar.gz", "r:gz")
     cli_tar.extractall(path=tmpdir)
     cli_tar.close()
+    copy2("cyclecloud", "/usr/local/bin")
 
     pogo_tar = tarfile.open("pogo-cli.linux64.tar.gz", "r:gz")
     pogo_tar.extractall(path=tmpdir)
     pogo_tar.close()
+    copy2("pogo", "/usr/local/bin")
 
     call(["cycle_server/install.sh", "--nostart"])
 
 
-def installPreReq():
+def install_pre_req():
     call(["yum", "install", "-y", "java-1.8.0-openjdk"])
 
 def main():
@@ -158,15 +269,22 @@ def main():
                       dest="cyclePortalPW",
                       help="Password for the ccount in the CycleCloud portal")
 
+    parser.add_argument("--cyclecloudAdminPW",
+                      dest="cyclecloudAdminPW",
+                      help="Admin user password for the cyclecloud application server")
+
     args = parser.parse_args()
 
-    installPreReq()
-    downloadAndInstallCC(args) 
-    ccLicense()
-    generateSSHkey()
-    modifyCSConfig()
-    startCC()
+    install_pre_req()
+    download_install_cc(args.downloadURL) 
+    generate_ssh_key()
+    modify_cs_config()
+    cc_license(args.licenseURL)
+    setup_azure_account(args.tenantId, args.applicationID, args.applicationSecret, args.cyclePortalAccount, args.cyclePortalPW, args.cyclecloudAdminPW)
+    start_cc()
+    setup_pogo_config()
 
+    clean_up()
 
 
 if __name__ == "__main__":
