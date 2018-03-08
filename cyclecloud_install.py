5712e42d6bff4bc8814b590fcaff197b03d5be39
Jer-Ming Chia <jer-ming.chia@cyclecomputing.com>
2018-03-07 17:27:47 -0800

    init fix

--- cyclecloud_install.py
+++ cyclecloud_install.py
@@ -120,8 +120,9 @@ def account_and_cli_setup(tenant_id, application_id, application_secret, cycle_p
     copy2(account_data_file, cycle_root + "/config/data/")
 
     # wait for the data to be imported
-    sleep(3)
-    _catch_sys_error(["/usr/local/bin/cyclecloud", "initialize", "--loglevel=debug", "--batch", "--url=https://localhost", "--verify-ssl=false", "--username=admin", ("--password='%s'" % cyclecloud_admin_pw)])    
+    password_flag = ("--password='%s'" % cyclecloud_admin_pw) 
+    sleep(5)
+    _catch_sys_error(["/usr/local/bin/cyclecloud", "initialize", "--loglevel=debug", "--batch", "--url=https://localhost", "--verify-ssl=false", "--username=admin", password_flag])    
 
     homedir = path.expanduser("~")
     cycle_config = homedir + "/.cycle/config.ini"
