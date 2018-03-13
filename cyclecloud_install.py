33a9975f602ac10139c59ef9524369490ef4387a
Jer-Ming Chia <jer-ming.chia@cyclecomputing.com>
2018-03-13 14:32:31 -0700

    Fix SSH dir permissions

--- cyclecloud_install.py
+++ cyclecloud_install.py
@@ -209,12 +209,12 @@ def generate_ssh_key():
     cs_sshkeyfile = cs_sshdir + "/cyclecloud.pem"
 
     if not path.isdir(cs_sshdir):
-        makedirs(cs_sshdir, mode=700)
+        makedirs(cs_sshdir)
     
     if not path.isdir(cs_sshkeyfile):
         copy2(sshkeyfile, cs_sshkeyfile)
         _catch_sys_error(["chown", "-R", "cycle_server.", cs_sshdir])
-        _catch_sys_error(["chown", "700", "cycle_server.", cs_sshdir])
+        _catch_sys_error(["chmod", "700", "cycle_server.", cs_sshdir])
 
 
 def cc_license(license_url):
